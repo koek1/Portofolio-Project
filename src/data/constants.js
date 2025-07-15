0af48b92270f1cebe41e617b52fc4af8aec51474
Geandre <80062438+koek1@users.noreply.github.com>
2025-07-15 16:41:29 +0200

    feat:Updated-CV

--- src/data/constants.js
+++ src/data/constants.js
@@ -16,7 +16,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/koek1",
   resume:
-    "https://drive.google.com/file/d/1Ia5eCmbEdSb7wOwLoy1kaugcCCuOYx1Q/view?usp=sharing",
+    "https://drive.google.com/file/d/1f3WEBc3ohOqx_wUcTk-AjsMc73bKauhK/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/geandr%C3%A9-du-preez-519428226/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/lasang_man/",
